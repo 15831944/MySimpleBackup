773e2ebed2284fcbd9aabe34c922116490211a07
TischlerWilly <oliver.schuft@gmx.de>
2019-02-10 15:21:57 +0100

    first commit b

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QDir>
+#include <QMessageBox>
+#include <QFile>
+#include "mydef.h"
+#include "textfunktionen.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +21,22 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_ziel_root_clicked();
+    void on_pushButton_ziel_clicked();
+    void on_lineEdit_ziel_root_editingFinished();
+
+    void on_actionInfo_triggered();
+
 private:
     Ui::MainWindow *ui;
+
+    void setup();
+    void schreibe_ini();
+    void info();
+
+    QString ziel_root;
+    QString ziel;
 };
 
 #endif // MAINWINDOW_H
